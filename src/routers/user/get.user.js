13cbbd4eb5d90afe069effcf9e4965c74cbc0bf7
unknown <maliksyah.28@gmail.com>
2022-11-10 22:13:07 +0700

    8

--- src/routers/user/get.user.js
+++ src/routers/user/get.user.js
@@ -16,3 +16,10 @@ const getUserList = async (req, res, next) => {
 };
 router.get("/userList", getUserList);
 module.exports = router;
+
+// config database heroku
+// "username": "yppfpbgtkxpfne",
+// "password": "38c4b1af1f09a03ba1b041b9f3652a0538d05972e2ea4c5c0a40c30856ea4bca",
+// "database": "d4ce2apk1ovrfg",
+// "host": "ec2-18-208-55-135.compute-1.amazonaws.com",
+// "dialect": "postgres"
