1e2f56ea7e69e82d5e3c6e9c9010b9926e08a8ba
unknown <maliksyah.28@gmail.com>
2022-11-10 20:59:04 +0700

    tes postgres heroku

--- config/config.js
+++ config/config.js
@@ -1,14 +1,24 @@
 require("dotenv").config();
 var postgres = require("pg");
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+const {
+  DB_HOST,
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME,
+  HerokuHost,
+  HerokuUser,
+  HerokuDatabase,
+  HerokuPort,
+  HerokuPassword,
+} = process.env;
 
 module.exports = {
   development: {
-    username: DB_USER,
-    password: "123Qazwsx!@#",
-    database: DB_NAME,
-    host: DB_HOST,
-    port: 5432,
+    username: DB_USER || HerokuUser,
+    password: "123Qazwsx!@#" || HerokuPassword,
+    database: DB_NAME || HerokuDatabase,
+    host: DB_HOST || HerokuHost,
+    port: 5432 || HerokuPort,
     dialect: "postgres",
 
     dialectOptions: {
